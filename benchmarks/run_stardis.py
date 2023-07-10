c2aff07d9712b6a9f17e74267c701d2762409217
Aarya Chaumal <aarya.chaumal@gmail.com>
2023-07-10 23:43:11 +0530

    Benchmark raytrace function

--- benchmarks/run_stardis.py
+++ benchmarks/run_stardis.py
@@ -3,7 +3,17 @@
 import os
 import numpy as np
 from stardis.base import run_stardis
+
 from astropy import units as u
+
+from tardis.io.atom_data import AtomData
+from tardis.io.config_validator import validate_yaml
+from tardis.io.config_reader import Configuration
+
+from stardis.model import read_marcs_to_fv
+from stardis.plasma import create_stellar_plasma
+from stardis.opacities import calc_alphas
+from stardis.transport import raytrace
 
 
 class BenchmarkRunStardis:
@@ -21,3 +31,51 @@ class BenchmarkRunStardis:
 
     def time_run_stardis(self):
         run_stardis(self.config, self.tracing_lambdas)
+
+class BenchmarkRaytrace:
+    """
+    Class to benchmark raytrace function.
+    """
+
+    def setup(self):
+        base_dir = os.path.abspath(os.path.dirname(__file__))
+        schema = os.path.join(base_dir, "config_schema.yml")
+        config_fname = os.path.join(base_dir, "benchmark_config.yml")
+        tracing_lambdas_or_nus = np.arange(6540, 6590, 0.01) * u.Angstrom
+        os.chdir(base_dir)
+
+        tracing_nus = tracing_lambdas_or_nus.to(u.Hz, u.spectral())
+
+        config_dict = validate_yaml(config_fname, schemapath=schema)
+        config = Configuration(config_dict)
+
+        adata = AtomData.from_hdf(config.atom_data)
+
+        stellar_model = read_marcs_to_fv(
+            config.model.fname,
+            adata,
+            final_atomic_number=config.model.final_atomic_number,
+        )
+        adata.prepare_atom_data(stellar_model.abundances.index.tolist())
+
+        stellar_plasma = create_stellar_plasma(stellar_model, adata)
+
+        alphas, gammas, doppler_widths = calc_alphas(
+            stellar_plasma=stellar_plasma,
+            stellar_model=stellar_model,
+            tracing_nus=tracing_nus,
+            opacity_config=config.opacity,
+        )
+
+        self.stellar_model = stellar_model
+        self.alphas = alphas
+        self.tracing_nus = tracing_nus
+        self.config = config
+
+    def time_raytrace(self):
+        raytrace(
+            self.stellar_model,
+            self.alphas,
+            self.tracing_nus,
+            no_of_thetas=self.config.no_of_thetas,
+        )
