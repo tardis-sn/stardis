33eab9af8c3c6ec005df9bb527a7cdda7302690d
William Black <wblack1845@gmail.com>
2023-07-10 14:37:34 -0700

    Delete prime number example

--- stardis/tests/test_example.py
+++ stardis/tests/test_example.py
@@ -1,22 +1,3 @@
-def test_primes():
-    from functools import reduce
-
-    # The following algorithm is the Sieve of Eratosthenes:
-    # https://stackoverflow.com/a/10640037
-
-    N = 30
-
-    assert reduce(
-        (
-            lambda r, x: [p for p in r if p not in list(range(x**2, N, x))]
-            if (x in r)
-            else r
-        ),
-        range(2, N),
-        list(range(2, N)),
-    ) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-
-
 def test_deprecation():
     import warnings
 
