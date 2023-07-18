88e118ff12dbe18aabc9b4d5b2f0005e313143d0
William Black <wblack1845@gmail.com>
2023-07-17 22:22:02 -0700

    Import module being tested inside test

--- stardis/opacities/tests/test_voigt.py
+++ stardis/opacities/tests/test_voigt.py
@@ -1,7 +1,6 @@
 import pytest
 from numpy import allclose, pi as PI
 from math import sqrt
-from stardis.opacities import voigt
 
 
 @pytest.mark.parametrize(
@@ -14,8 +13,10 @@ from stardis.opacities import voigt
 def test_faddeeva_sample_values(
     faddeeva_sample_values_input, faddeeva_sample_values_expected_result
 ):
+    from stardis.opacities.voigt import faddeeva
+
     assert allclose(
-        voigt.faddeeva(faddeeva_sample_values_input),
+        faddeeva(faddeeva_sample_values_input),
         faddeeva_sample_values_expected_result,
     )
 
@@ -34,8 +35,10 @@ def test_voigt_profile_division_by_zero(
     voigt_profile_input_doppler_width,
     voigt_profile_input_gamma,
 ):
+    from stardis.opacities.voigt import voigt_profile
+
     with pytest.raises(ZeroDivisionError):
-        _ = voigt.voigt_profile(
+        _ = voigt_profile(
             voigt_profile_input_delta_nu,
             voigt_profile_input_doppler_width,
             voigt_profile_input_gamma,
@@ -55,8 +58,10 @@ def test_voigt_profile_sample_values(
     voigt_profile_input_gamma,
     voigt_profile_expected_result,
 ):
+    from stardis.opacities.voigt import voigt_profile
+
     assert allclose(
-        voigt.voigt_profile(
+        voigt_profile(
             voigt_profile_input_delta_nu,
             voigt_profile_input_doppler_width,
             voigt_profile_input_gamma,
