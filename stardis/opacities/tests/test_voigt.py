91653ecb3f6f8d895416f34299e1091795bf67d1
William Black <125844868+smokestacklightnin@users.noreply.github.com>
2023-07-28 21:47:11 -0700

    Add faddeeva_cuda test

--- stardis/opacities/tests/test_voigt.py
+++ stardis/opacities/tests/test_voigt.py
@@ -1,8 +1,11 @@
 import pytest
-from numpy import allclose, pi as PI
+import numpy as np
 from math import sqrt
+from numba import cuda
+from stardis.opacities.voigt import faddeeva, faddeeva_cuda, voigt_profile
 
-from stardis.opacities.voigt import faddeeva, voigt_profile
+# Test cases must also take into account use of a GPU to run. If there is no GPU then the test cases will fail.
+GPUs_available = cuda.is_available()
 
 
 @pytest.mark.parametrize(
@@ -10,13 +13,38 @@ from stardis.opacities.voigt import faddeeva, voigt_profile
     [
         (0, 1 + 0j),
         (0.0, 1.0 + 0.0j),
+        (np.array([0, 0]), np.array([1 + 0j, 1 + 0j])),
     ],
 )
 def test_faddeeva_sample_values(
     faddeeva_sample_values_input, faddeeva_sample_values_expected_result
 ):
-    assert allclose(
+    assert np.allclose(
         faddeeva(faddeeva_sample_values_input),
+        faddeeva_sample_values_expected_result,
+    )
+
+
+@pytest.mark.skipif(
+    not GPUs_available, reason="No GPU is available to test CUDA function"
+)
+@pytest.mark.parametrize(
+    "faddeeva_sample_values_input, faddeeva_sample_values_expected_result",
+    [
+        (0, 1 + 0j),
+        (0.0, 1.0 + 0.0j),
+        (np.array([0, 0]), np.array([1 + 0j, 1 + 0j])),
+    ],
+)
+def test_faddeeva_cuda_sample_values(
+    faddeeva_sample_values_input, faddeeva_sample_values_expected_result
+):
+    test_values = cuda.to_device(faddeeva_sample_values_input)
+    result_values = cuda.device_array_like(test_values)
+
+    faddeeva_cuda.forall(len(test_values))(result_values, test_values)
+    assert np.allclose(
+        result_values.copy_to_host,
         faddeeva_sample_values_expected_result,
     )
 
@@ -57,8 +85,8 @@ def test_voigt_profile_division_by_zero(
 @pytest.mark.parametrize(
     "voigt_profile_sample_values_input_delta_nu, voigt_profile_sample_values_input_doppler_width, voigt_profile_sample_values_input_gamma, voigt_profile_sample_values_expected_result",
     [
-        (0, 1, 0, 1 / sqrt(PI)),
-        (0, 2, 0, 1 / (sqrt(PI) * 2)),
+        (0, 1, 0, 1 / sqrt(np.pi)),
+        (0, 2, 0, 1 / (sqrt(np.pi) * 2)),
     ],
 )
 def test_voigt_profile_sample_values_sample_values(
@@ -67,7 +95,7 @@ def test_voigt_profile_sample_values_sample_values(
     voigt_profile_sample_values_input_gamma,
     voigt_profile_sample_values_expected_result,
 ):
-    assert allclose(
+    assert np.allclose(
         voigt_profile(
             voigt_profile_sample_values_input_delta_nu,
             voigt_profile_sample_values_input_doppler_width,
