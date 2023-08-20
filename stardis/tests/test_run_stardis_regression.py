e2d78d72c0e1ac6c681bb2099c0e5d2b0948caec
Aarya Chaumal <aarya.chaumal@gmail.com>
2023-08-20 13:02:42 +0530

    Add regression for alphas

--- stardis/tests/test_run_stardis_regression.py
+++ stardis/tests/test_run_stardis_regression.py
@@ -1,9 +1,14 @@
 from stardis import run_stardis
 import numpy as np
 from astropy import units as u
+import os
 
 
-def test_run_stardis_regression(data_regression):
+def test_run_stardis_regression(ndarrays_regression):
     tracing_lambdas = np.arange(6540, 6590, 0.01) * u.Angstrom
-    sim_result = run_stardis("stardis_example.yml", tracing_lambdas)
-    data_regression.check(sim_result)
+    config_fname = "benchmarks/benchmark_config.yml"
+    base_dir = os.path.abspath(os.path.dirname(config_fname))
+    config_fname = os.path.basename(config_fname)
+    os.chdir(base_dir)
+    sim_result = run_stardis(config_fname, tracing_lambdas)
+    ndarrays_regression.check({"alphas": sim_result.alphas})
