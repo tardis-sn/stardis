ac586990d3c3d82eb6456a5ab53868e16c1af2d6
Joshua Shields <54691495+jvshields@users.noreply.github.com>
2023-09-11 15:58:07 -0400

    add w2 in the small and large delta tau case (#132)

--- stardis/radiation_field/radiation_field_solvers/base.py
+++ stardis/radiation_field/radiation_field_solvers/base.py
@@ -22,9 +22,11 @@ def calc_weights(delta_tau):
     if delta_tau < 5e-4:
         w0 = delta_tau * (1 - delta_tau / 2)
         w1 = delta_tau**2 * (0.5 - delta_tau / 3)
+        w2 = delta_tau**3 * (1 / 3 - delta_tau / 4)
     elif delta_tau > 50:
         w0 = 1.0
         w1 = 1.0
+        w2 = 2.0
     else:
         exp_delta_tau = np.exp(-delta_tau)
         w0 = 1 - exp_delta_tau
