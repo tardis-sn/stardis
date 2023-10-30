003f797baf8b6b08ba93e8ab0e4b3f86ed2349d4
William Black <125844868+smokestacklightnin@users.noreply.github.com>
2023-10-30 16:44:50 -0300

    Change location of restructured `tardis` libraries in `benchmarks/run_stardis.py`

--- benchmarks/run_stardis.py
+++ benchmarks/run_stardis.py
@@ -6,7 +6,7 @@ from stardis.base import run_stardis
 from astropy import units as u
 
 from tardis.io.atom_data import AtomData
-from tardis.io.config_validator import validate_yaml
+from tardis.io.configuration.config_validator import validate_yaml
 from tardis.io.config_reader import Configuration
 
 from stardis.io.model.marcs import read_marcs_model
