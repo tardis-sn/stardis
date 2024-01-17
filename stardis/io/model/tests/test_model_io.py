91305018edf4596123fb3e05e731780e609d0e76
Hayden James Monk <monkhayd@moria.egr.msu.edu>
2024-01-17 12:31:31 -0500

    Added create_model function, changed all functions to use create_model, deleted existing fixture

--- stardis/io/model/tests/test_model_io.py
+++ stardis/io/model/tests/test_model_io.py
@@ -8,57 +8,53 @@ from importlib_resources import files
 
 from stardis.io.model.marcs import read_marcs_model
 
-
 @pytest.fixture
-def marcs_model_test_data_file_path():
+def create_model():
     fname = "marcs_test.mod.gz"
     fpath = files("stardis.io.model.tests.data")
-    return fpath.joinpath(fname)
+    test_data_filepath= fpath.joinpath(fname)
+    model = read_marcs_model(test_data_filepath)
+    return  model
 
 
-def test_read_marcs_model_scaled_log_number_fraction(marcs_model_test_data_file_path):
+def test_read_marcs_model_scaled_log_number_fraction(create_model):
     """
     Test reading a MARCS model file
     """
-    model = read_marcs_model(marcs_model_test_data_file_path)
 
     assert np.allclose(
-        model.data.scaled_log_number_fraction_1,
+        create_model.data.scaled_log_number_fraction_1,
         12.0,
     )
 
 
-def test_read_marcs_model_metadata_surface_grav(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
+def test_read_marcs_model_metadata_surface_grav(create_model):
 
     assert np.allclose(
-        model.metadata["surface_grav"].value,
+        create_model.metadata["surface_grav"].value,
         10000,
     )
 
 
-def test_read_marcs_model_metadata_x(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
-
+def test_read_marcs_model_metadata_x(create_model):
+    
     assert np.allclose(
-        model.metadata["x"],
+        create_model.metadata["x"],
         0.73826,
     )
 
 
-def test_read_marcs_model_data_depth(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
-
+def test_read_marcs_model_data_depth(create_model):
+    
     assert np.allclose(
-        model.data.depth.iloc[-1],
+        create_model.data.depth.iloc[-1],
         44610000.0,
     )
 
 
-def test_read_marcs_model_data_lgtaur(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
-
+def test_read_marcs_model_data_lgtaur(create_model):
+    
     assert np.allclose(
-        model.data.lgtaur.iloc[0],
+        create_model.data.lgtaur.iloc[0],
         -5.0,
     )
