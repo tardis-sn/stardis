7a0548c6f2dc06b38869571374eca6943023b6e8
Hayden James Monk <monkhayd@moria.egr.msu.edu>
2024-02-14 11:16:35 -0500

    Added MARCS_TEST_FPATH, removed redundancies

--- stardis/io/model/tests/test_model_io.py
+++ stardis/io/model/tests/test_model_io.py
@@ -14,23 +14,9 @@ MARCS_TEST_FPATH = Path(__file__).parent / "data" / "marcs_test.mod.gz"
 MESA_TEST_FPATH = Path(__file__).parent / "data" / "end_core_h_burn.mod"
 
 
-@pytest.fixture
-def marcs_model_test_data_file_path():
-    fname = "marcs_test.mod.gz"
-    fpath = files("stardis.io.model.tests.data")
-    return fpath.joinpath(fname)
-
-
 @pytest.fixture(scope="session")
-def marcs_model_test_data():
-    return read_marcs_model(marcs_model_test_data_file_path)
-
-
-@pytest.fixture(scope="session")
-def mesa_model_test_data_file_path():
-    fname = "end_core_h_burn.mod"
-    fpath = files("stardis.io.model.tests.data")
-    return fpath.joinpath(fname)
+def marcs_model():
+    return read_marcs_model(MARCS_TEST_FPATH)
 
 
 @pytest.fixture(scope="session")
@@ -60,11 +46,11 @@ def test_mesa_truncation(mesa_model):
     assert len(mesa_model.data) == len_before_truncation - 1
 
 
-def test_read_marcs_model_scaled_log_number_fraction(marcs_model_test_data_file_path):
+def test_read_marcs_model_scaled_log_number_fraction(marcs_model_file_path):
     """
     Test reading a MARCS model file
     """
-    model = read_marcs_model(marcs_model_test_data_file_path)
+    model = read_marcs_model(marcs_model_file_path)
 
     assert np.allclose(
         model.data.scaled_log_number_fraction_1,
@@ -72,8 +58,8 @@ def test_read_marcs_model_scaled_log_number_fraction(marcs_model_test_data_file_
     )
 
 
-def test_read_marcs_model_metadata_surface_grav(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
+def test_read_marcs_model_metadata_surface_grav(marcs_model_file_path):
+    model = read_marcs_model(marcs_model_file_path)
 
     assert np.allclose(
         model.metadata["surface_grav"].value,
@@ -81,8 +67,8 @@ def test_read_marcs_model_metadata_surface_grav(marcs_model_test_data_file_path)
     )
 
 
-def test_read_marcs_model_metadata_x(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
+def test_read_marcs_model_metadata_x(marcs_model_file_path):
+    model = read_marcs_model(marcs_model_file_path)
 
     assert np.allclose(
         model.metadata["x"],
@@ -90,8 +76,8 @@ def test_read_marcs_model_metadata_x(marcs_model_test_data_file_path):
     )
 
 
-def test_read_marcs_model_data_depth(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
+def test_read_marcs_model_data_depth(marcs_model_file_path):
+    model = read_marcs_model(marcs_model_file_path)
 
     assert np.allclose(
         model.data.depth.iloc[-1],
@@ -99,8 +85,8 @@ def test_read_marcs_model_data_depth(marcs_model_test_data_file_path):
     )
 
 
-def test_read_marcs_model_data_lgtaur(marcs_model_test_data_file_path):
-    model = read_marcs_model(marcs_model_test_data_file_path)
+def test_read_marcs_model_data_lgtaur(marcs_model_file_path):
+    model = read_marcs_model(marcs_model_file_path)
 
     assert np.allclose(
         model.data.lgtaur.iloc[0],
