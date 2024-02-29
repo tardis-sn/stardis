4b54e0c48578f617a1ed99b9615d4fe4259fbc55
Josh Shields <shield90@msu.edu>
2024-02-29 16:12:47 -0500

    compare the correct stardis outputs

--- stardis/tests/test_stardis_full.py
+++ stardis/tests/test_stardis_full.py
@@ -15,11 +15,13 @@ def test_stardis_broadening_output(
 
 
 def test_stardis_parallel_output(
-    example_stardis_output_parallel, example_tracing_nus, example_stardis_output
+    example_stardis_output_parallel,
+    example_tracing_nus,
+    example_stardis_output_broadening,
 ):
     assert len(example_stardis_output_parallel.spectrum_nu) == len(example_tracing_nus)
     assert ~np.all(np.isnan(example_stardis_output_parallel.spectrum_nu))
     assert_allclose(
         example_stardis_output_parallel.spectrum_nu,
-        example_stardis_output.spectrum_nu,
+        example_stardis_output_broadening.spectrum_nu,
     )
