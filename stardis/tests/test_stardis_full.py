ed2a6705cdce44061fec9dfd7014f0bb9fa3f68e
Joshua Shields <54691495+jvshields@users.noreply.github.com>
2024-03-04 11:55:09 -0500

    fix bug with last depth point out of bounds error (#178)
    * fix bug with last depth point out of bounds error
    
    * make tau j+1 = 0
    
    * fix comment to align with tau = 0
    
    * rewrite the equation to use array broadcasting
    
    * try numba optimization and parallelization
    
    * add parallel implementation
    
    * add parallelization support
    
    * add parallel test
    
    * small single threaded optimization and add conftest
    
    * testing benchmarks
    
    * fix tests
    
    * try njitting single-threaded weight calculation
    
    * fix parallelization bug
    
    * further simplification of single threaded loop
    
    * apply black
    
    * final polish
    
    * unwrap thetas for the broadcasting case
    
    * check parallel output against serial output
    
    * compare the correct stardis outputs

--- stardis/tests/test_stardis_full.py
+++ stardis/tests/test_stardis_full.py
@@ -1,3 +1,7 @@
+import numpy as np
+from numpy.testing import assert_allclose
+
+
 def test_stardis_output(example_stardis_output, example_tracing_nus):
     assert len(example_stardis_output.spectrum_nu) == len(example_tracing_nus)
 
@@ -7,4 +11,17 @@ def test_stardis_broadening_output(
 ):
     assert len(example_stardis_output_broadening.spectrum_nu) == len(
         example_tracing_nus
+    )
+
+
+def test_stardis_parallel_output(
+    example_stardis_output_parallel,
+    example_tracing_nus,
+    example_stardis_output_broadening,
+):
+    assert len(example_stardis_output_parallel.spectrum_nu) == len(example_tracing_nus)
+    assert ~np.all(np.isnan(example_stardis_output_parallel.spectrum_nu))
+    assert_allclose(
+        example_stardis_output_parallel.spectrum_nu,
+        example_stardis_output_broadening.spectrum_nu,
     )
