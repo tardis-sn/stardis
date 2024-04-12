8ec5fc0c6a949b01949b8120acd3ad845e5fa5fe
Josh Shields <shields.joshua.v@gmail.com>
2024-04-12 10:58:03 -0400

    checkpoint

--- stardis/model/composition/base.py
+++ stardis/model/composition/base.py
@@ -16,3 +16,22 @@ class Composition:
     def __init__(self, density, atomic_mass_fraction):
         self.density = density
         self.atomic_mass_fraction = atomic_mass_fraction
+
+    def rescale_from_element_fraction(self, element, scale_factor):
+        """
+        Renormalizes the composition after multiplying the specified element by the scale factor.
+
+        Args:
+            element: The element to multiply.
+            scale_factor: How much to rescale the element by.
+
+        Returns:
+            Composition: The rescaled composition object.
+        """
+        new_atomic_mass_fraction = self.atomic_mass_fraction.copy()
+        new_atomic_mass_fraction.loc[element] = (
+            new_atomic_mass_fraction.loc[element] * scale_factor
+        )
+        self.atomic_mass_fraction = new_atomic_mass_fraction.div(
+            new_atomic_mass_fraction.sum(axis=0)
+        )
