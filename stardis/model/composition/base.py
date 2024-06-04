8041edaf5034df4da9ec0fc3bb4f91d7a822191d
Josh Shields <shields.joshua.v@gmail.com>
2024-06-04 14:39:53 -0400

    accept lists of elements

--- stardis/model/composition/base.py
+++ stardis/model/composition/base.py
@@ -1,3 +1,6 @@
+from tardis.util.base import element_symbol2atomic_number
+
+
 class Composition:
     """
     Holds composition information
@@ -17,7 +20,7 @@ class Composition:
         self.density = density
         self.atomic_mass_fraction = atomic_mass_fraction
 
-    def rescale_from_element_fraction(self, element, scale_factor):
+    def rescale_from_element_fraction(self, elements, scale_factors):
         """
         Renormalizes the composition after multiplying the specified element by the scale factor.
 
@@ -28,10 +31,27 @@ class Composition:
         Returns:
             Composition: The rescaled composition object.
         """
+
+        # If elements and scale_factors are not lists, make them lists
+        if not isinstance(elements, list):
+            elements = [elements]
+        if not isinstance(scale_factors, list):
+            scale_factors = [scale_factors]
+
+        if len(elements) != len(scale_factors):
+            raise ValueError(
+                "The lists elements and scale_factors should have the same length."
+            )
+
         new_atomic_mass_fraction = self.atomic_mass_fraction.copy()
-        new_atomic_mass_fraction.loc[element] = (
-            new_atomic_mass_fraction.loc[element] * scale_factor
-        )
+
+        for element, scale_factor in zip(elements, scale_factors):
+            if not isinstance(element, int):
+                element = element_symbol2atomic_number(element)
+            new_atomic_mass_fraction.loc[element] = (
+                new_atomic_mass_fraction.loc[element] * scale_factor
+            )
+
         self.atomic_mass_fraction = new_atomic_mass_fraction.div(
             new_atomic_mass_fraction.sum(axis=0)
         )
