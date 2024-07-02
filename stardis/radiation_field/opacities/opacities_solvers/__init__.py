6532f9678538e8bf9c0092ba969099a2153d1241
Josh Shields <shields.joshua.v@gmail.com>
2024-07-02 12:59:38 -0400

    blackify new import

--- stardis/radiation_field/opacities/opacities_solvers/__init__.py
+++ stardis/radiation_field/opacities/opacities_solvers/__init__.py
@@ -1,2 +1,4 @@
 from stardis.radiation_field.opacities.opacities_solvers.base import *
-from stardis.radiation_field.opacities.opacities_solvers.broadening import rotation_broadening
+from stardis.radiation_field.opacities.opacities_solvers.broadening import (
+    rotation_broadening,
+)
